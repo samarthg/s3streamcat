653206f1f914706c819eb328cf9448a5c08e5b29
Samarth Gahire <samarth.gahire@gmail.com>
2017-03-22 19:59:10 +0530

    [PyPI] Changes in the process of publishing to PyPI.

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(name='s3streamcat',
       version='0.1.0',
       description='Streaming based unix cat like functionality for file on S3',
-      url='',
+      url='https://github.com/samarthg/s3streamcat',
+      download_url = '',
       author='Samarth Gahire',
       author_email='samarth.gahire@gmail.com',
-      license='',
+      license='MIT',
       packages=['s3streamcat', 's3client'],
       install_requires=[
           'boto3==1.3.1',
@@ -17,4 +18,5 @@ setup(name='s3streamcat',
               's3streamcat = s3streamcat.s3streamcat:stream_s3_file'
           ]
       },
+      keywords = ['s3', 'print', 'aws', 'cat', 'stream'],
       )
